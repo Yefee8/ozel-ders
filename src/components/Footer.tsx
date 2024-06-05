ed2f23014a5086538e97fa1b13d6cffeb4bc8b00
yefee8 <yavuzefefindikli8@gmail.com>
2024-06-05 17:39:59 +0300

    fix: -

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-      <footer className="w-full n min-h-16 items-center justify-center flex border-t-primary/10 border-t">
-        <div className="container justify-between max-sm:justify-center flex gap-16 max-md:gap-8 flex-wrap items-center max-md:px-6 py-6">
+      <footer className="w-full min-h-16 items-center justify-center flex border-t-primary/10 border-t">
+        <div className="container  justify-between max-sm:justify-center flex gap-16 max-md:gap-8 flex-wrap items-center md:px-0 px-4 py-6">
           <h1 className="text-2xl font-medium text-primary">Özel Ders</h1>
   
           <div className="flex max-sm:justify-center flex-wrap gap-8 max-md:gap-4">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="w-full min-h-16 items-center flex justify-center border-b-primary/10 border-b">
-      <div className="container max-sm:justify-between flex gap-16 max-md:gap-8 flex-wrap items-center max-md:px-6 py-6">
+      <div className="container w-full md:px-0 px-4 justify-between md:justify-start flex gap-16 max-md:gap-8 flex-wrap items-center  py-6">
         <h1 className="text-2xl font-medium text-primary">Özel Ders</h1>
 
         <div className="flex max-sm:justify-center flex-wrap gap-8 max-md:gap-4">
