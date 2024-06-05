e1e7be69d0ac29fd8c9d27104a3049d0152159d4
yefee8 <yavuzefefindikli8@gmail.com>
2024-06-05 17:37:33 +0300

    fix: responsive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens:{
+      'xl': '1500px',
+    },
     extend: {
       colors: {
         primary: "#1a2130",
